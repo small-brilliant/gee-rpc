db90e2cf81f6cbed73b049958dd6877c6786ee06
smallBrilliant <wh_199610@163.com>
2022-06-15 19:21:49 +0800

    day01

---
+++ day1-codec/server.go
@@ -0,0 +1,141 @@
+package geerpc
+
+import (
+	"encoding/json"
+	"fmt"
+	"geerpc/day1-codec/codec"
+	"io"
+	"log"
+	"net"
+	"reflect"
+	"sync"
+)
+
+const MagicNumber = 0x3bef5c
+
+//Option 采用 JSON 编码 Option，后续的 header 和 body 的编码方式由 Option 中的 CodeType 指定
+type Option struct {
+	MagicNumber int        //MagicNumber marks this's a geerpc request
+	CodecType   codec.Type // client may choose different Codec to encode body
+}
+
+var DefaultOption = &Option{
+	MagicNumber: MagicNumber,
+	CodecType:   codec.GobType,
+}
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+var DefaultServer = NewServer()
+
+func (server *Server) Accept(lis net.Listener) {
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error:", err)
+			return
+		}
+		go server.ServeConn(conn)
+	}
+}
+func Accept(lis net.Listener) { DefaultServer.Accept(lis) }
+
+// ServeConn 首先使用 json.NewDecoder 反序列化得到 Option 实例, 检查 MagicNumber 和 CodeType 的值是否正确。
+// 然后根据 CodeType 得到对应的消息编解码器，接下来的处理交给 serverCodec
+func (server *Server) ServeConn(conn io.ReadWriteCloser) {
+	defer func() { _ = conn.Close() }()
+	var opt Option
+	if err := json.NewDecoder(conn).Decode(&opt); err != nil {
+		log.Println("rpc server: options error: ", err)
+		return
+	}
+	if opt.MagicNumber != MagicNumber {
+		log.Printf("rpc server: invalid magic number &x", opt.MagicNumber)
+		return
+	}
+	f := codec.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		log.Printf("rpc server: invalid codec type %s", opt.CodecType)
+		return
+	}
+	server.serveCodec(f(conn))
+}
+
+// invalidRequest 出错时返回这个
+var invalidRequest = struct{}{}
+
+// serveCodec 主要包含三个阶段
+// 读取请求 readRequest
+// 处理请求 handleRequest
+// 回复请求 sendResponse
+func (server *Server) serveCodec(cc codec.Codec) {
+	sending := new(sync.Mutex)
+	wg := new(sync.WaitGroup)
+	// 无限制地等待请求的到来
+	for {
+		req, err := server.readRequest(cc)
+		if err != nil {
+			//尽力而为，只有在 header 解析失败时，才终止循环
+			if req == nil {
+				break // 无法恢复，关闭连接
+			}
+			req.h.Error = err.Error()
+			server.sendResponse(cc, req.h, invalidRequest, sending)
+			continue
+		}
+		wg.Add(1)
+		go server.handleRequest(cc, req, sending, wg)
+	}
+	wg.Wait()
+	_ = cc.Close()
+}
+
+//request 存储通信的所有信息
+type request struct {
+	h            *codec.Header // 头部信息
+	argv, replyv reflect.Value
+}
+
+func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
+	var h codec.Header
+	if err := cc.ReadHeader(&h); err != nil {
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
+			log.Println("rpc server: read header error:", err)
+		}
+		return nil, err
+	}
+	return &h, nil
+}
+func (server *Server) readRequest(cc codec.Codec) (*request, error) {
+	h, err := server.readRequestHeader(cc)
+	if err != nil {
+		return nil, err
+	}
+	req := &request{h: h}
+	// TODO: now we don't know the type of request argv
+	// day 1, just suppose it's string
+	req.argv = reflect.New(reflect.TypeOf(""))
+	if err = cc.ReadBody(req.argv.Interface()); err != nil {
+		log.Println("rpc server: read argv err:", err)
+	}
+	return req, nil
+}
+func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
+	sending.Lock()
+	defer sending.Unlock()
+	if err := cc.Write(h, body); err != nil {
+		log.Println("rpc server: write response error:", err)
+	}
+}
+func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
+	// TODO, should call registered rpc methods to get the right replyv
+	// day 1, just print argv and send a hello message
+	defer wg.Done()
+	log.Println(req.h, req.argv.Elem())
+	req.replyv = reflect.ValueOf(fmt.Sprintf("geerpc resp %d", req.h.Seq))
+	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+}
---
+++ test/sysM_test.go
@@ -0,0 +1,42 @@
+package test
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+var set = make(map[int]bool, 0)
+
+func printOnce(index int, num int) {
+
+	if _, ok := set[num]; !ok {
+		fmt.Println(index, num)
+	}
+	set[num] = true
+}
+
+func TestPrint(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		go printOnce(i, 100)
+	}
+	time.Sleep(time.Second)
+}
+
+func TestPrint2(t *testing.T) {
+	var num = 0
+	var locker sync.Mutex
+
+	for i := 0; i < 100000; i++ {
+		go func(i int) {
+			locker.Lock()
+			defer locker.Unlock()
+
+			num++
+			fmt.Printf("goroutine %d: num = %d\n", i, num)
+		}(i)
+	}
+
+	time.Sleep(time.Second)
+}
